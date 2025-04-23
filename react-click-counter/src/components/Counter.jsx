e1cfe98d6fbb0fc4df8d88df35fdac579b54802f
renatojo <renatojose.mo@gmail.com>
2025-04-23 20:09:34 -0300

    modificação no css do contador

--- react-click-counter/src/components/Counter.jsx
+++ react-click-counter/src/components/Counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import './CounterCss.css'
 
 const Counter = () => {
     const [estado1, setEstado1] = useState(0);
@@ -9,11 +10,14 @@ const Counter = () => {
             <main>
             
             <h1>Contador de Cliques</h1>
-            <button onClick={() => setEstado1(estado1 + 1)}>{estado1}</button>
-            <button onClick={() => setEstado1(estado1 - 1)}>Adicione -1 ao time A</button>
+            <div id="DivContadorPrincipal">
 
-            <button onClick={() => setEstado2(estado2 + 1)}>{estado2}</button>
-            <button onClick={() => setEstado2(estado2 - 1)}>Adicione +1 ao time B</button>
+            <button class="ContadorPrincipal" onClick={() => setEstado1(estado1 + 1)}>{estado1}</button>
+            <button class="ContadorPrincipal" onClick={() => setEstado2(estado2 + 1)}>{estado2}</button>
+
+            </div>
+            <button  onClick={() => setEstado1(estado1 - 1)}>Adicione -1 ao time A</button>
+            <button onClick={() => setEstado2(estado2 - 1)}>Adicione -1 ao time B</button>
             <button onClick={() => {
                  setEstado2(0);
                  setEstado1(0);
