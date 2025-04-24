a2cef935542c2bbe373a75b62ea689df936bc0ba
renatojo <renatojose.mo@gmail.com>
2025-04-24 19:27:31 -0300

    a

--- react-click-counter/src/components/Counter.jsx
+++ react-click-counter/src/components/Counter.jsx
@@ -11,13 +11,17 @@ const Counter = () => {
             
             <h1>Contador de Cliques</h1>
 
+            <div id="DivTitulos">
+                <h1 class="titulos" id="casa">CASA</h1>
+                <h1 class="titulos" id="visitante">VISITANTE</h1>
+            </div>
+
             <div id="DivContadorPrincipal">
             <button class="BotaoDiminuirQuantidade" onClick={() => setEstado1(estado1 - 1)}>-1</button>
             <button class="ContadorPrincipal" onClick={() => setEstado1(estado1 + 1)}>{estado1}</button>
             <button class="ContadorPrincipal" onClick={() => setEstado2(estado2 + 1)}>{estado2}</button>
             <button class="BotaoDiminuirQuantidade" onClick={() => setEstado2(estado2 - 1)}>-1</button>
             </div>
-
 
             <button onClick={() => {
                  setEstado2(0);
